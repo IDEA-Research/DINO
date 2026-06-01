e4ac98b27cbb50695b55b80e5e36ec18c92585a1
Hung2512 <120157318+Hung2512@users.noreply.github.com>
2026-06-01 14:25:43 +0700

    Update slconfig.py

--- util/slconfig.py
+++ util/slconfig.py
@@ -314,7 +314,7 @@ class SLConfig(object):
             based_on_style='pep8',
             blank_line_before_nested_class_or_def=True,
             split_before_expression_after_opening_paren=True)
-        text, _ = FormatCode(text, style_config=yapf_style, verify=True)
+        text, _ = FormatCode(text, style_config=yapf_style)
 
         return text
     
